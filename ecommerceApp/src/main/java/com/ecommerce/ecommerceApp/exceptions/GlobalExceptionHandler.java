432f282be2cdfdfa70d2b8f940240ba621569188
Zee Mo <ziad.dessoki@yahoo.com>
2025-01-03 17:29:09 -0500

    GlobalExeptionHandler

---
+++ ecommerceApp/src/main/java/com/ecommerce/ecommerceApp/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.ecommerce.ecommerceApp.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.ecommerce.ecommerceApp.dto.ErrorResponse;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleProductNotFoundException(ProductNotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
