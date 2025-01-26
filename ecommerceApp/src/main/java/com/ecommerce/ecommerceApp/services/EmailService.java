75f2f0d040e82a9a4b97449c4bcacb8d9ba7a924
Zee Mo <ziad.dessoki@yahoo.com>
2025-01-25 21:27:55 -0500

    email service

---
+++ ecommerceApp/src/main/java/com/ecommerce/ecommerceApp/services/EmailService.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ecommerce.ecommerceApp.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public String sendEmail(String to, String subject, String text) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        message.setFrom("javamailer@fastmail.com");
+        try {
+
+            javaMailSender.send(message);
+            logger.info("Email sent successfully." + message.getFrom());
+            return "Email sent successfully!";
+
+        } catch (MailException ex) {
+            // simply log it and go on...
+            logger.error(ex.getMessage() + message.getFrom());
+            return ex.getMessage();
+        }
+    }
+}
