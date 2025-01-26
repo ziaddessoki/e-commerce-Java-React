c964c1ed803e525bcb8793c2b83d08a49aca7330
Zee Mo <ziad.dessoki@yahoo.com>
2025-01-25 21:25:20 -0500

    product controller update

--- ecommerceApp/src/main/java/com/ecommerce/ecommerceApp/controllers/ProductController.java
+++ ecommerceApp/src/main/java/com/ecommerce/ecommerceApp/controllers/ProductController.java
@@ -12,12 +12,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.ecommerceApp.model.Product;
 import com.ecommerce.ecommerceApp.services.ProductServices;
 
 @RestController
+@RequestMapping("/api")
 public class ProductController {
 
     private final ProductServices productServices;
