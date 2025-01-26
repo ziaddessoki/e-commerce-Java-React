b6b0b5b92161b227db9a787fdc3d58275dcc5bd1
Zee Mo <ziad.dessoki@yahoo.com>
2025-01-25 21:27:05 -0500

    product model

--- ecommerceApp/src/main/java/com/ecommerce/ecommerceApp/model/Product.java
+++ ecommerceApp/src/main/java/com/ecommerce/ecommerceApp/model/Product.java
@@ -4,31 +4,42 @@
  */
 package com.ecommerce.ecommerceApp.model;
 
+import java.util.Date;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 @Table(name = "PRODUCT", schema = "PRODUCTS")
 public class Product {
 
     @Id
-    private int productId;
-    @NotEmpty(message = "File name is required")
-    private String productName;
+    private int id;
+    @NotEmpty(message = "Product name is required")
+    private String name;
+    private String description;
+    private String brand;
     private String price;
+    private String category;
+    private Date releaseDate;
+    private boolean available;
+    private int quantity;
 
-    public Product() {
-    }
-
+    //we use @NoArgsConstructor instead of
+    // public Product() {
+    // }
+    // we use @AllArgsConstructor instead of
     // public Product(int productId, String productName, String price) {
     //     this.productId = productId;
     //     this.productName = productName;
